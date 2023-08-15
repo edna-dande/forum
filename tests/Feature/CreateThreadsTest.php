633876272a2ff7d4cd6c853c852fa91181f6e0e7
edna-dande <dandeedna@gmail.com>
2023-08-15 17:13:55 +0300

    testing helpers

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -15,14 +15,17 @@ class CreateThreadsTest extends TestCase
     {
         $this->expectException('Illuminate\Auth\AuthenticationException');
 
-        $thread = factory('App\Thread')->make();
+        $thread = make('App\Thread');
+
         $this->post('/threads', $thread->toArray());
     }
     function test_an_authenticated_user_can_create_new_forum_threads()
     {
-        $this->actingAs(factory('App\User')->create());
+//        $this->actingAs(create('App\User'));
+        $this->signIn();
 
-        $thread = factory('App\Thread')->make();
+        $thread = make('App\Thread');
+
         $this->post('/threads', $thread->toArray());
 
         $this->get($thread->path())
