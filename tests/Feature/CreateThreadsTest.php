30ee56806326f94b0711ba0bc7d6d14915197470
edna-dande <dandeedna@gmail.com>
2023-10-02 17:34:34 +0300

    a user can delete their threads

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -55,6 +55,38 @@ class CreateThreadsTest extends TestCase
         $this->publishThread(['channel_id' => 999])
             ->assertSessionHasErrors('channel_id');
     }
+
+    function test_guests_cannot_delete_threads()
+    {
+        $this->withExceptionHandling();
+        $thread = create('App\Thread');
+
+        $response = $this->delete($thread->path());
+
+        $response->assertRedirect('/login');
+
+    }
+    function test_a_thread_can_be_deleted()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $reply = create('App\Reply', ['thread_id' => $thread->id]);
+
+        $response = $this->json('DELETE', $thread->path());
+
+        $response->assertStatus(204);
+
+        $this->assertDatabaseMissing('threads', ['id' =>$thread->id]);
+        $this->assertDatabaseMissing('replies', ['id' =>$reply->id]);
+
+    }
+
+    function test_threads_may_only_be_deleted_by_those_who_have_permission()
+    {
+
+    }
     public function publishThread($overrides = [])
     {
         $this->withExceptionHandling()->signIn();
