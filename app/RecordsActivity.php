793b3880d4d4a76702a96121d384e0ffa3a20892
edna-dande <dandeedna@gmail.com>
2023-10-12 10:33:02 +0300

    the activity deletion bug

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -12,6 +12,9 @@ trait RecordsActivity
                 $model->recordActivity($event);
             });
         }
+        static::deleting(function ($model) {
+            $model->activity()->delete();
+        });
     }
 
     protected static function getActivitiesToRecord()
